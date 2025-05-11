d762d55d63828e79366b4896f92331c89d1cd660
Diego <diegocayax202231074@cunoc.edu.gt>
2025-05-10 22:31:47 -0600

    informacion agregada

--- Proyecto1/obj/Debug/net8.0/Proyecto1.AssemblyInfo.cs
+++ Proyecto1/obj/Debug/net8.0/Proyecto1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+baadecad3e80d9f0e93f4ffa6010723d11f9cef6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4386d9e1a8f7f2c24369cb503a7a50643f97f857")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
